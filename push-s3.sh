694fd154cc121e041b63c6aec593b86241d59de7
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-07-11 12:18:00 -0400

    Include version in log

--- push-s3.sh
+++ push-s3.sh
@@ -11,9 +11,10 @@ git diff --quiet || die 'Uncommitted changes: Stash or commit before pushing dem
 BRANCH=`git rev-parse --abbrev-ref HEAD`
 DATE=`date "+%Y-%m-%d"`
 HASH=`git rev-parse --short HEAD`
+VERSION=`jq -r .version package.json`
 DEST_PATH="vitessce-data/vitessce-api/$DATE/$HASH/openapi.json"
 
 aws s3 cp web_deploy/openapi.json "s3://$DEST_PATH"
 DEST_URL="https://s3.amazonaws.com/$DEST_PATH"
 
-echo "| $DATE | $BRANCH | [$HASH](https://github.com/hms-dbmi/vitessce-api/tree/$HASH) | [html](https://redocly.github.io/redoc/?url=$DEST_URL) | [json]($DEST_URL) |" >> README.md
+echo "| $DATE | $BRANCH | $VERSION | [$HASH](https://github.com/hms-dbmi/vitessce-api/tree/$HASH) | [html](https://redocly.github.io/redoc/?url=$DEST_URL) | [json]($DEST_URL) |" >> README.md
